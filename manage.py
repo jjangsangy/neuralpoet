a06ce80a0516d2baf8cc80d7bc925c9c0e66ecc2
Sang Han <jjangsangy@gmail.com>
2016-08-05 16:52:50 -0400

    added new requirements

--- manage.py
+++ manage.py
@@ -1,10 +1,15 @@
 import os
 
 from flask_script import Manager
+from flask_debugtoolbar import DebugToolbarExtension
 
 from neuralpoet import app
 
+app.debug = True
+
 manager = Manager(app)
+toolbar = DebugToolbarExtension(app)
+
 
 if __name__ == "__main__":
     manager.run()
