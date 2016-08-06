00cc78491975a57146b64b937b5f0c80636cade8
Sang Han <jjangsangy@gmail.com>
2016-08-06 01:36:16 -0400

    configuration

--- config.py
+++ config.py
@@ -6,7 +6,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class BaseConfig(object):
     DEBUG = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'E8OuPFR6aHogiH3gJunU6nNIFNJFCZRKmI4k'
     ASSETS_DEBUG = True
 
 
--- neuralpoet/__init__.py
+++ neuralpoet/__init__.py
@@ -2,20 +2,22 @@
 Neural Poet
 ===========
 """
-__title__ = 'neuralpoet'
-__license__ = 'Apache Software License Version 2.0'
-
 import os
 
 from flask import Flask
-from flask_cors import CORS
-
 from flask_bootstrap import Bootstrap
+from flask_cors import CORS
 from flask_heroku import Heroku
 
 from .views.site import site
 
-app = Flask(__name__)
+__title__ = 'neuralpoet'
+__license__ = 'Apache Software License Version 2.0'
+
+app = Flask(__name__, instance_relative_config=True)
+
+if os.path.exists('config.py'):
+    app.config.from_pyfile('config.py')
 
 app.register_blueprint(site)
 app.config.from_object(os.environ['APP_SETTINGS'])
